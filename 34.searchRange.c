6d960fcc1f5c0f23772838a58fecd79179d69634
harini242006 <harinipetchi242006@gmail.com>
2026-08-19 14:59:27 +0530

    Implement searchRange function to find target indices

---
+++ 34.searchRange.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = (int*)malloc(2 * sizeof(int));
+    *returnSize = 2;
+    result[0] = -1;
+    result[1] = -1;
+
+    if (numsSize == 0) {
+        return result;
+    }
+
+    int left = 0, right = numsSize - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            result[0] = mid;
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    if (result[0] == -1) {
+        return result;
+    }
+
+    left = 0; 
+    right = numsSize - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            result[1] = mid;
+            left = mid + 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return result;
+}
